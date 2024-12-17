e8a7bca9ad1166ce84d7ff75501e8d3b62317fc5
mehmetfatihbezan@gmail.com <mehmetfatihbezan@gmail.com>
2024-12-18 00:44:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+const headerId=document.querySelector("#header");
+headerId.style.backgroundColor="yellow";
+headerId.style.color="green";
+
+const pageTitleId=document.querySelector("#page_title");
+pageTitleId.style.backgroundColor="red";
+pageTitleId.style.color="pink";
+
+const contentId=document.querySelector("#content");
+contentId.style.backgroundColor="blue";
+contentId.style.color="yellow"
+
+const footertId=document.querySelector("#footer");
+footertId.style.backgroundColor="green";
+footertId.style.color="blue"
+
